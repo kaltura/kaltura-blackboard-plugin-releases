ffa03adab146f57fef8c4597b6fafa093c0ed0d8
Gonen Radai <gonen.radai@kaltura.com>
2022-03-15 20:05:49 +0200

    added page

--- apps/kaltura/src/app/app.tsx
+++ apps/kaltura/src/app/app.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
-
+import { ThemeProvider } from '@kep-ui-kit/ui-theme';
+import {ReactComponent as Logo}  from './logo.svg';
+import { Typography, Table, TableContainer, TableHead, TableCell, TableRow, TableBody, Button  } from '@kep-ui-kit/ui-components';
 const StyledApp = styled.div`
   // Your style here
 `;
@@ -8,7 +9,26 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="kaltura" />
+      <ThemeProvider>
+        <Logo></Logo>
+        <Typography variant='h1'>Kaltura Releases - Download Page</Typography>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell>Releases Link</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Blackboard</TableCell>
+                <TableCell><Button href="https://github.com/kaltura/releases/tree/blackboard-releases">Download</Button></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </ThemeProvider>
     </StyledApp>
   );
 }
