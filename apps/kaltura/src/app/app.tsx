d1fef12521cf9a760444d99cabfbea71eb4501b6
Gonen Radai <gonen.radai@kaltura.com>
2022-03-17 10:22:38 +0200

    change to reflect new release method

--- apps/kaltura/src/app/app.tsx
+++ apps/kaltura/src/app/app.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { ThemeProvider } from '@kep-ui-kit/ui-theme';
 import {ReactComponent as Logo}  from './logo.svg';
-import { Typography, Table, TableContainer, TableHead, TableCell, TableRow, TableBody, Button  } from '@kep-ui-kit/ui-components';
+import { Typography, Button  } from '@kep-ui-kit/ui-components';
 const StyledApp = styled.div`
   // Your style here
 `;
@@ -11,23 +11,10 @@ export function App() {
     <StyledApp>
       <ThemeProvider>
         <Logo></Logo>
-        <Typography variant='h1'>Kaltura Releases - Download Page</Typography>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Product</TableCell>
-                <TableCell>Releases Link</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Blackboard</TableCell>
-                <TableCell><Button href="https://github.com/kaltura/releases/tree/blackboard-releases">Download</Button></TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Typography variant='h2'>Kaltura Blackboard BuildingBlock - Plugin Download</Typography>
+        <Typography variant='body1'>Click the button below to go to the releases page in github.</Typography>
+        <Typography variant='body1'>Download the .war file from the latest release that is listed there.</Typography>
+        <Button href="https://github.com/kaltura/kaltura-blackboard-plugin-releases/releases">Download</Button>
       </ThemeProvider>
     </StyledApp>
   );
